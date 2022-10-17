25132ba1a2080fbb9411525381494764b14c1414
Salman0112358 <Salman0112358@outlook.com>
2022-10-17 16:49:22 +0100

    Added onclick handlers for setting a cross or nought when clicking a square button

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {ImCross} from 'react-icons/im'
+import {BiCircle} from 'react-icons/bi'
+type Player = 'noughts' | 'crosses' | null
 
-const Square = () => {
+interface ISquare {
+    value : Player,
+    onClick : () => void,
+    winner : Player,
+
+}
+
+const Square = ({value, onClick, winner} : ISquare) => {
+
+    if (!value) return <button className='square' onClick={onClick} disabled={Boolean(winner)}/>
+
   return (
-    <button>This is supposed to be a single square button</button>
+    <button className={`square square_${value.toLocaleLowerCase()}`} disabled>{value=== 'crosses' ? <ImCross/> : <BiCircle/> }</button>
   )
 }
 
--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
+import Square from "../components/Square";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [currentPlayer, setCurrentPlayer] = useState<'noughts' | 'crosses'>((Math.round(Math.random() * 1) === 1 ? 'noughts' : 'crosses'))
+  const [currentPlayer, setCurrentPlayer] = useState<"noughts" | "crosses">(
+    Math.round(Math.random() * 1) === 1 ? "noughts" : "crosses"
+  );
+  const [winner, setWinner] = useState(null);
 
-  return <div>Board</div>;
+  const setSquareValue = (index : number) => {
+    const newData = squares.map((value, i) => {
+        if (i === index ){
+            return currentPlayer;
+        }
+        return value;
+    })
+    setSquares(newData)
+    setCurrentPlayer(currentPlayer === 'noughts' ? 'crosses' : 'noughts' )
+  }
+
+  return (
+    <>
+      <h3>{`It's your move ${currentPlayer}`}</h3>
+      <div className="grid">
+      {Array(9)
+        .fill(null)
+        .map((_, index) => {
+          return (
+              <Square 
+              key={index} 
+              onClick={() => setSquareValue(index)}
+              value={squares[index]}
+              winner={winner}
+              />
+          )
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Board;
