aca83eeb8d732cf4a20452059556e588eb2bc27b
Salman0112358 <Salman0112358@outlook.com>
2022-10-17 17:26:29 +0100

    More styling and winning logic condition added to a function

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Square from "../components/Square";
 
+type Player = 'noughts' | 'crosses' | null
+
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState<"noughts" | "crosses">(
@@ -23,6 +25,26 @@ const Board = () => {
     setSquares(Array(9).fill(null));
     setWinner(null);
     setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "noughts" : "crosses");
+  }
+
+  const findWinner = (squares : Player[]) => {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let item of lines){
+        const [a,b,c] = item
+        if(squares[a] && squares[a] === squares[b] && squares[a]===squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
   }
 
   return (
